648f88b24513a8727c8a6a65177edaa40e216331
azinchen <azinchen@student.hive.fi>
2025-06-13 01:50:52 +0300

    started ex00

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -12,4 +12,37 @@
 
 #include "Fixed.hpp"
 
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+}
 
+Fixed::Fixed(const Fixed& other) : _value(other._value)
+{
+	std::cout << "Copy constructor called" << std::endl;
+};
+Fixed& Fixed::operator=(const Fixed& other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+	{
+		_value = other._value;
+	}
+	return *this;
+};
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	
+}
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -13,6 +13,20 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+# include <iostream>
 
+class Fixed
+{
+	private:
+		int					_value;
+		static const int	_fractBits;
+	public:
+		Fixed();
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+};
 
 #endif
