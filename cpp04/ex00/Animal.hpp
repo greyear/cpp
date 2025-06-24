48ae7df2fe5389161326dacdc18f4623c120b5f8
azinchen <azinchen@student.hive.fi>
2025-06-25 01:38:39 +0300

    oops started cpp04 not ex04

---
+++ cpp04/ex00/Animal.hpp
@@ -0,0 +1,21 @@
+
+//headers
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <string>
+# include <iostream>
+
+class Animal
+{
+	protected:
+		std::string type;
+	public:
+		Animal();
+		Animal(const Animal& other);
+		Animal& operator=(const Animal& other);
+		~Animal(); //virtual?
+};
+
+#endif
