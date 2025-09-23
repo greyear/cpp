10b44e29ac88a2a0589af84653152c4f99891674
Anna Zinchenko <anna.s.zinchenko@gmail.com>
2025-09-24 01:21:05 +0300

    reading about casting types

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -17,3 +17,25 @@ class ScalarConverter
 	public:
 		static convert(const std::string& )
 };
+
+/*
+1) static_cast <new_type> (exp);
+
+used to convert between related types, such as numeric types or pointers
+char c = 'a';
+int i = static_cast<int>(c);
+
+2) dynamic_cast <new_type> (exp);
+
+returns a null pointer (for pointer conversions) or throws a bad_cast exception (for reference conversions).
+Animal* animalPtr = new Dog();
+Dog* dogPtr = dynamic_cast<Dog*>(animalPtr) будет не нулл
+Cat* catPtr = dynamic_cast<Cat*>(animalPtr) будет нулл!
+
+3) const_cast <new_type> (exp);
+
+allows programmers to temporarily remove the constancy of an object and make modifications
+
+
+typeid() operator to check the data type
+*/
