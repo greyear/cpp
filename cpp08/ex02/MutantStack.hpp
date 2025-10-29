1f100c3df3eb4e06120e8bfd39d2850891eb45a9
azinchen <azinchen@student.hive.fi>
2025-10-29 14:46:14 +0200

    ex02 tests are done

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azinchen <azinchen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 13:09:19 by azinchen          #+#    #+#             */
+/*   Updated: 2025/10/29 13:09:20 by azinchen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #pragma once
 
@@ -7,58 +18,58 @@
 template <typename T, class Container = std::deque<T>>
 class MutantStack : public std::stack<T, Container>
 {
-    public:
+	public:
 
-        using std::stack<T, Container>::stack;
-        //Container is type of the container used (by default deque)
-        //typename Container::iterator is TYPE of iterator for that container type
-        using iterator = typename Container::iterator;
-        using const_iterator = typename Container::const_iterator;
-        using reverse_iterator = typename Container::reverse_iterator;
-        using const_reverse_iterator = typename Container::const_reverse_iterator;
+		using std::stack<T, Container>::stack;
+		//Container is type of the container used (by default deque)
+		//typename Container::iterator is TYPE of iterator for that container type
+		using iterator = typename Container::iterator;
+		using const_iterator = typename Container::const_iterator;
+		using reverse_iterator = typename Container::reverse_iterator;
+		using const_reverse_iterator = typename Container::const_reverse_iterator;
 
-        MutantStack() = default;
-        MutantStack(const MutantStack& other) = default;
-        MutantStack& operator=(const MutantStack& other) = default;
-        ~MutantStack() = default;
+		MutantStack() = default;
+		MutantStack(const MutantStack& other) = default;
+		MutantStack& operator=(const MutantStack& other) = default;
+		~MutantStack() = default;
 
-        iterator begin()
-        {
-            return (this->c.begin());
-        }
+		iterator begin()
+		{
+			return (this->c.begin());
+		}
 
-        iterator end()
-        {
-            return (this->c.end());
-        }
+		iterator end()
+		{
+			return (this->c.end());
+		}
 
-        const_iterator begin() const
-        {
-            return (this->c.begin());
-        }
+		const_iterator begin() const
+		{
+			return (this->c.begin());
+		}
 
-        const_iterator end() const
-        {
-            return (this->c.end());
-        }
+		const_iterator end() const
+		{
+			return (this->c.end());
+		}
 
-        reverse_iterator rbegin()
-        {
-            return (this->c.rbegin());
-        }
+		reverse_iterator rbegin()
+		{
+			return (this->c.rbegin());
+		}
 
-        reverse_iterator rend()
-        {
-            return (this->c.rend());
-        }
+		reverse_iterator rend()
+		{
+			return (this->c.rend());
+		}
 
-        const_reverse_iterator rbegin() const
-        {
-            return (this->c.rbegin());
-        }
+		const_reverse_iterator rbegin() const
+		{
+			return (this->c.rbegin());
+		}
 
-        const_reverse_iterator rend() const
-        {
-            return (this->c.rend());
-        } 
+		const_reverse_iterator rend() const
+		{
+			return (this->c.rend());
+		} 
 };
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,9 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azinchen <azinchen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 13:09:03 by azinchen          #+#    #+#             */
+/*   Updated: 2025/10/29 13:09:05 by azinchen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
 #include <iostream>
 
 int main()
 {
+	std::cout << "\n1) TEST FROM SUBJECT " << std::endl;
 	//creating stack
 	MutantStack<int> mstack;
 
@@ -12,13 +25,13 @@ int main()
 	mstack.push(17);
 
 	//printing top element
-	std::cout << mstack.top() << std::endl;
+	std::cout << "top element after adding 5 and then 17: " << mstack.top() << std::endl;
 
 	//deleting top element
 	mstack.pop();
 
 	//printing size after
-	std::cout << mstack.size() << std::endl;
+	std::cout << "size after popping 1 element: " << mstack.size() << std::endl;
 
 	//adding 4 more elements
 	mstack.push(3);
@@ -32,16 +45,72 @@ int main()
 
 	//movind iterators
 	++it;
+	std::cout << "*it after ++it: " << *it << std::endl;
 	--it;
+	std::cout << "*it after --it: " << *it << std::endl;
 
-	//printing all elements
+	//printing all elements of mstack using our created iterator
+	std::cout << "elements of mstack: " << std::endl;
 	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		std::cout << "element of mstack: " << *it << std::endl;
 		++it;
 	}
 
-	//creating real stack
+	//creating real stack by copying
 	std::stack<int> s(mstack);
+
+	//printing all elements of s by taking the top and popping (reverse order!)
+	std::cout << "\nelements of s (in reverse): " << std::endl;
+	while (!s.empty())
+	{
+		std::cout << "element of stack: " << s.top() << std::endl;
+		s.pop();
+	}
+
+	//////////////////////////////////////////////
+
+	//test with list instead of mstack
+	std::cout << "\n2) TEST WITH LIST " << std::endl;
+	//creating list
+	std::list<int> ls;
+
+	//adding 2 elements
+	ls.push_back(5);
+	ls.push_back(17);
+
+	//printing last element
+	std::cout << "last element after adding 5 and then 17: " << ls.back() << std::endl;
+
+	//deleting last element
+	ls.pop_back();
+
+	//printing size after
+	std::cout << "size after popping 1 element: " << ls.size() << std::endl;
+
+	//adding 4 more elements
+	ls.push_back(3);
+	ls.push_back(5);
+	ls.push_back(737);
+	ls.push_back(0);
+
+	//creating iterators
+	std::list<int>::iterator itL = ls.begin();
+	std::list<int>::iterator iteL = ls.end();
+
+	//movind iterators
+	++itL;
+	std::cout << "*itL after ++itL: " << *itL << std::endl;
+	--itL;
+	std::cout << "*itL after --itL: " << *itL << std::endl;
+
+	//printing all elements of list
+	std::cout << "elements of list: " << std::endl;
+	while (itL != iteL)
+	{
+		std::cout << "element of list: " << *itL << std::endl;
+		++itL;
+	}
+
 	return 0;
 }
